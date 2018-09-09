79a5614498e69823c93830970260205e5e40c3a8
mace <mace_blue@hotmail.com>
2018-09-09 18:15:23 +0200

    add conf

--- loungetable.py
+++ loungetable.py
@@ -2,6 +2,17 @@ import math, sys, time, random, colorsys, pygame, socket
 from pygame.locals import *
 from colorsys import hsv_to_rgb, rgb_to_hsv
 from neopixel import *
+
+
+# LED strip configuration:
+LED_COUNT      = 150      # Number of LED pixels.
+LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
+LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
+LED_BRIGHTNESS = 55     # Set to 0 for darkest and 255 for brightest
+LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+
 class LoungeTable:
     REFRESHSCREEN = USEREVENT+1
     spidev = file("/dev/spidev0.0", "wb")
