41c5c50c2f06595e5a5e7cff19b75fa2a317de38
mace <mace_blue@hotmail.com>
2018-10-01 19:31:25 +0200

    return color array

--- mytetris2.py
+++ mytetris2.py
@@ -1436,13 +1436,13 @@ class RGB_Tetris:
     def wheel(self,pos):
         """Generate rainbow colors across 0-255 positions."""
         if pos < 85:
-            return Color(pos * 3, 255 - pos * 3, 0)
+            return [pos * 3, 255 - pos * 3, 0] #Color(pos * 3, 255 - pos * 3, 0)
         elif pos < 170:
             pos -= 85
-            return Color(255 - pos * 3, 0, pos * 3)
+            return [255 - pos * 3, 0, pos * 3] #Color(255 - pos * 3, 0, pos * 3)
         else:
             pos -= 170
-            return Color(0, pos * 3, 255 - pos * 3)
+            return [0, pos * 3, 255 - pos * 3] #Color(0, pos * 3, 255 - pos * 3)
 
     def rainbowDriveGameOver(self):
         self.rainbowDriveRunning = False
